86a2201f5f08e661ab2ac0a63297eb2d9dca876d
Rafie1715 <rojagatrafie@gmail.com>
2024-11-21 22:50:59 +0700

    initial commit

--- app/src/main/java/com/dicoding/planetkuapp/ui/register/RegisterActivity.kt
+++ app/src/main/java/com/dicoding/planetkuapp/ui/register/RegisterActivity.kt
@@ -1,10 +1,11 @@
-package com.dicoding.planetkuapp
+package com.dicoding.planetkuapp.ui.register
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.dicoding.planetkuapp.R
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
