c0e5858d41bcb83f63112831977bc58a15d56da9
Rafie1715 <rojagatrafie@gmail.com>
2024-11-22 17:05:01 +0700

    Initial commit

--- app/src/main/java/com/dicoding/planetkuapp/ui/register/RegisterActivity.kt
+++ app/src/main/java/com/dicoding/planetkuapp/ui/register/RegisterActivity.kt
@@ -1,21 +1,41 @@
 package com.dicoding.planetkuapp.ui.register
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.dicoding.planetkuapp.R
+import com.dicoding.planetkuapp.databinding.ActivityRegisterBinding
+import com.dicoding.planetkuapp.ui.login.LoginActivity
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegisterBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_register)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // Inisialisasi View Binding
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Aksi tombol register
+        binding.btnRegister.setOnClickListener {
+            val email = binding.etEmail.text.toString().trim()
+            val password = binding.etPassword.text.toString().trim()
+
+            if (email.isNotEmpty() && password.isNotEmpty()) {
+                Toast.makeText(this, "Register berhasil! Silakan login.", Toast.LENGTH_SHORT).show()
+
+                val intent = Intent(this, LoginActivity::class.java)
+                startActivity(intent)
+                finish()
+            } else {
+                Toast.makeText(this, "Email dan password tidak boleh kosong!", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
